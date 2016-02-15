f14c5e7635bee3703702a59776c229942c35d035
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-02-15 10:45:00 -0500

    adding list of models and reorganizing analyses

--- analyses_fire_yucatan.R
+++ analyses_fire_yucatan.R
@@ -6,7 +6,7 @@
 #
 #AUTHOR: Benoit Parmentier, Marco Millones                                                                      #
 #DATE CREATED: 02/06/2016 
-#DATE MODIFIED: 02/12/2016
+#DATE MODIFIED: 02/14/2016
 #Version: 1
 #PROJECT: Land cover Change Yucatan, Marco Millones
 #   
@@ -39,7 +39,7 @@ library(mlogit)                          # maximum liklihood estimation and mult
 
 ###### Functions used in this script
 
-functions_analyses_script <- "analyses_fire_yucatan_functions_02122016.R" #PARAM 1
+functions_analyses_script <- "analyses_fire_yucatan_functions_02142016.R" #PARAM 1
 script_path <- "/home/bparmentier/Google Drive/FireYuca_2016/R_scripts" #path to script #PARAM 2
 source(file.path(script_path,functions_analyses_script)) #source all functions used in this script 1.
 
@@ -56,14 +56,40 @@ CRS_reg <- CRS_WGS84 # PARAM 4
 file_format <- ".rst" #PARAM5
 NA_value <- -9999 #PARAM6
 NA_flag_val <- NA_value #PARAM7
-out_suffix <-"yucatan_analyses_02062016" #output suffix for the files and ouptu folder #PARAM 8
+out_suffix <-"yucatan_analyses_02142016" #output suffix for the files and ouptu folder #PARAM 8
 create_out_dir_param=TRUE #PARAM9
-
+state_fname <- "/home/bparmentier/Google Drive/FireYuca_2016/IN_QGIS/State_dis_from_muni.shp"
 data_Hansen_fname <- "/home/bparmentier/Google Drive/FireYuca_2016/Hansen_fire.xlsx" #contains the whole dataset
 #data_CI_fname <- "/home/bparmentier/Google Drive/FireYuca_2016/yucatan_fire.xlsx" #contains the whole dataset
 data_CI_fname <- "/home/bparmentier/Google Drive/FireYuca_2016/yucatan_fire.csv" #contains the whole dataset
 #data_CI_02062016.txt
 coord_names <- c("POINT_X","POINT_Y") #PARAM 11
+zonal_var_name <- "state" #name of the variable to use to run the model by zone, Yucatan state here
+y_var_name <- "fpnfpch"
+ref_var_name <- ""
+run_relevel <- TRUE
+
+list_models<-c("y_var ~ cy_q",
+               "y_var ~ cy_q + FIRE_pre07",
+               "y_var ~ cy_q + FIRE_intensity",
+               "y_var ~ cy_q + FIRE_bool",
+               "y_var ~ cy_q +  
+                      dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
+                      elevation + slope_deg + landcover + cattledensity + ejido + 
+                      popdens_change + precip + protegidas + soil",
+               "y_var ~ cy_q + FIRE_pre07 + 
+                      dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
+                      elevation + slope_deg + landcover + cattledensity + ejido + 
+                      popdens_change + precip + protegidas + soil",
+               "y_var ~ cy_q + FIRE_intensity + 
+                      dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
+                      elevation + slope_deg + landcover + cattledensity + ejido + 
+                      popdens_change + precip + protegidas + soil",
+               "y_var ~ cy_q + FIRE_freq + 
+                      dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
+                      elevation + slope_deg + landcover + cattledensity + ejido + 
+                      popdens_change + precip + protegidas + soil"
+)
 
 ################# START SCRIPT ###############################
 
@@ -86,27 +112,47 @@ if(create_out_dir_param==TRUE){
 #data_Hansen <-read.xls(data_Hansen_fname, sheet=1)
 #data_CI <-read.xls(data_CI_fname, sheet=1)
 #data_Hansen <-read.xls(data_Hansen_fname, sheet=1)
-data_CI <-read.table(data_CI_fname,stringsAsFactors=F,header=T,sep=",")
+
+data_df <-read.table(data_CI_fname,stringsAsFactors=F,header=T,sep=",")
 
 #write.table(data_Hansen,file.path(in_dir,"data_Hansen_02062016.txt"),sep=",")
 #write.table(data_CI,file.path(in_dir,"data_CI_02062016.txt"),header=T,sep=",")
 
-data_CI_spdf <- data_CI
-coordinates(data_CI_spdf) <- coord_names
-
-### Quick check of plotting variables
-spplot(data_CI_spdf,"cattledensity")
-spplot(data_CI_spdf,"FIRE_pre07")
-spplot(data_CI_spdf,"dist_roads")
-
 #Create the count variable
 fire_modis_col <- c("FIRE_2000","FIRE_2001","FIRE_2002","FIRE_2003","FIRE_2004","FIRE_2005","FIRE_2006","FIRE_2007")
-data_CI$FIRE_freq <- colSums(data_CI[,fire_modis_col])
-data_CI$FIRE_intensity <- data_CI$FIRE_freq/8
-data_CI$FIRE_bool <- data_CI$FIRE_freq > 0
+data_df$FIRE_freq <- colSums(data_df[,fire_modis_col])
+data_df$FIRE_intensity <- data_df$FIRE_freq/8
+data_df_spdf$FIRE_bool <- data_df$FIRE_freq > 0
 
-hist(data_CI$fpnfpch)
-table(data_CI$fpnfpch)
+data_df_spdf <- data_df
+coordinates(data_df_spdf) <- coord_names
+filename<-sub(extension(basename(state_fname)),"",basename(state_fname))       #Removing path and the extension from file name.
+state_outline <- readOGR(dsn=dirname(state_fname), filename)
+
+proj4string(data_df_spdf) <- proj4string(state_outline)
+l_poly <- over(data_df_spdf,state_outline)
+data_df_spdf <- cbind(data_df_spdf,l_poly)
+coordinates(data_df_spdf) <- coord_names
+#unique(data_df_spdf$FIRST_NOM_)
+data_df_spdf$state <- as.character(data_df_spdf$FIRST_NOM_)
+table(data_df_spdf$state)
+sum(table(data_df_spdf$state))
+nrow(data_df_spdf)
+#> sum(table(data_df_spdf$state))
+#[1] 136311
+#> nrow(data_df_spdf)
+#[1] 136904
+
+### Quick check of plotting variables
+spplot(data_df_spdf,"cattledensity")
+spplot(data_df_spdf,"FIRE_pre07")
+spplot(data_df_spdf,"dist_roads")
+spplot(data_df_spdf,"state")
+
+
+hist(data_df[[y_var_name]])
+table(data_df[[y_var_name]])
+barplot(table(data_df[[y_var_name]]))
 
 #### Run modeling series A ###
 
@@ -114,66 +160,32 @@ table(data_CI$fpnfpch)
 
 #make this a function?
 
-list_models<-c("y_var ~ cy_q",
-               "y_var ~ cy_q + FIRE_pre07",
-               "y_var ~ cy_q + FIRE_intensity",
-               "y_var ~ cy_q + FIRE_bool",
-               "y_var ~ cy_q +  
-                      dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
-                      elevation + slope_deg + landcover + cattledensity + ejido + 
-                      popdens_change + precip + protegidas + soil",
-               "y_var ~ cy_q + FIRE_pre07 + 
-                      dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
-                      elevation + slope_deg + landcover + cattledensity + ejido + 
-                      popdens_change + precip + protegidas + soil",
-               "y_var ~ cy_q + FIRE_intensity + 
-                      dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
-                      elevation + slope_deg + landcover + cattledensity + ejido + 
-                      popdens_change + precip + protegidas + soil",
-               "y_var ~ cy_q + FIRE_freq + 
-                      dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
-                      elevation + slope_deg + landcover + cattledensity + ejido + 
-                      popdens_change + precip + protegidas + soil"
-               )
+#debug(run_multinom_mod)
+if(run_relevel==TRUE){
+  data_df_spdf[[y_var_name]] <- as.factor(data_df_spdf[[y_var_name]])
+}
+unique_val_zones <- (unique(data_df_spdf[[zonal_var_name]]))
+unique_val_zones <- unique_val_zones[!is.na(unique_val_zones)]
+unique_val_y_var <- (unique(data_df_spdf[[y_var_name]]))
+unique_val_y_var <- order(unique_val_zones[!is.na(unique_val_y_var)],decreasing = TRUE) #values 1,2,3
 
-y_var_name <- "fpnfpch"
-debug(run_multinom_mod)
-list_mod <- run_multinom_mod(list_models,model_type="multinom",y_var_name ,data_df=data_CI)
+#lapply() loop through unique val zones later!!
+#subset(data_df, state=="")
+#ref_var_name <- 1
 
-  
-# mod1a <- multinom(fpnfpch ~ cy_q , data = data_CI)
-# mod2a <- multinom(fpnfpch ~ cy_q + FIRE_pre07, data = data_CI)
-# mod3a <- multinom(fpnfpch ~ cy_q + FIRE_intensity, data = data_CI)
-# mod4a <- multinom(fpnfpch ~ cy_q + FIRE_bool, data = data_CI)
-#   
-# mod5a <- multinom(fpnfpch ~ cy_q +  
-#                       dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
-#                       elevation + slope_deg + landcover + cattledensity + ejido + 
-#                       popdens_change + precip + protegidas + soil
-#                     , data = data_CI)
-#   
-# mod6a <- multinom(fpnfpch ~ cy_q + FIRE_pre07 + 
-#                       dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
-#                       elevation + slope_deg + landcover + cattledensity + ejido + 
-#                       popdens_change + precip + protegidas + soil
-#                     , data = data_CI)
-#   
-# mod7a <- multinom(fpnfpch ~ cy_q + FIRE_intensity + 
-#                       dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
-#                       elevation + slope_deg + landcover + cattledensity + ejido + 
-#                       popdens_change + precip + protegidas + soil
-#                     , data = data_CI)
-#   
-# mod8a <- multinom(fpnfpch ~ cy_q + FIRE_freq + 
-#                       dist_disturbed + dist_loc + dist_rur + dist_urb + dist_roads + 
-#                       elevation + slope_deg + landcover + cattledensity + ejido + 
-#                       popdens_change + precip + protegidas + soil
-#                     , data = data_CI)
-  
+list_mod_obj <- vector("list",length=length(unique_val_y_var))
+for(i in 1:length(unique_val_y_var)){
+  ref_var_name <- unique_val_y_var[i]
+  #debug(run_multinom_mod)
+  list_mod_obj[[i]] <- run_multinom_mod(list_models,model_type="multinom",y_var_name,data_df=data_df_spdf,ref_var_name=ref_var_name)
+}
+
 mod_names <-  c("mod1a","mod2a","mod3a","mod4a","mod5a","mod6a","mod7a","mod8a")
 df_mod <- data.frame(mod_names=mod_names,AIC=AIC_values)
 barplot(df_mod$AIC,names="mod_names",names.arg=mod_names)
-
+#debug(extraction_of_information)
+test <- extraction_of_information(list_mod)
+  
 ##Note that multinom does not calculate p-values but we can carry out a Wald test...
 
 #> mod2a$coefnames
@@ -183,12 +195,12 @@ barplot(df_mod$AIC,names="mod_names",names.arg=mod_names)
 #2  -0.1389069 -0.382720  0.2230844
 #3  -2.2844927 -3.746468  0.1316694
 
-z <- summary(mod8a)$coefficients/summary(mod8a)$standard.errors
-z
+#z <- summary(mod8a)$coefficients/summary(mod8a)$standard.errors
+#z
 
 #2-tailed z test
-p <- (1 - pnorm(abs(z), 0, 1))*2
-p
+#p <- (1 - pnorm(abs(z), 0, 1))*2
+#p
 
 ############### END OF SCRIPT ###################
 
